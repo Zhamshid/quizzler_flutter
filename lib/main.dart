aed662ccca59ed3420f68972f629e5e0f375d2a0
Zhamshid Irisbayev <180103263@stu.sdu.edu.kz>
2021-09-22 23:48:03 +0600

    verstka

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuizzlerPage(),
+    home: Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: QuizPage(),
+        ),
+      ),
+    ),
   ));
 }
 
-class QuizzlerPage extends StatelessWidget {
-  const QuizzlerPage({Key? key}) : super(key: key);
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  _QuizPageState createState() => _QuizPageState();
 }
 
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'There will be any quiestions',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              color: Colors.green,
+              onPressed: () {},
+              child: Text(
+                'TRUE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              color: Colors.red,
+              onPressed: () {
+              },
+              child: Text(
+                'FALSE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
